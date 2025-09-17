15b1d972a2e11674b94667d636e7d41618d61dab
DanielSnadon <danielsnadon@gmail.com>
2025-09-17 13:48:24 +0300

    ClearFiles

--- Task1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/snadon/OOP_Labs/Task1
-# Build directory: /home/snadon/OOP_Labs/Task1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Task1Tests "/home/snadon/OOP_Labs/Task1/build/run_tests")
-set_tests_properties(Task1Tests PROPERTIES  _BACKTRACE_TRIPLES "/home/snadon/OOP_Labs/Task1/CMakeLists.txt;18;add_test;/home/snadon/OOP_Labs/Task1/CMakeLists.txt;0;")
